0f3e916d6936e5f1555e26600ec4c3e5dca4e21a
Shreyansh Srivastava <shreyansh.official.6726@gmail.com>
2026-06-14 16:31:24 +0530

    work

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL;
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:7860";
 
 const api = axios.create({
   baseURL: API_BASE,
